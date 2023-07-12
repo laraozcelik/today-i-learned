11989afab8da8a8ad7bec64fc685164136fc5a23
Lara Ozcelik <ozceliklara@gmail.com>
2023-07-12 12:51:52 -0400

    API key security

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hezswwmcchcvgkvoponc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhlenN3d21jY2hjdmdrdm9wb25jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg2NTc4ODgsImV4cCI6MjAwNDIzMzg4OH0.lG6GNsdfXsQ3NOZE2w4f-iiLgREZ7nYGNu-IIKzSNWA";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
