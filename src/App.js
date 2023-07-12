1f189768262608d0917f820bff72ddf4317c8058
Lara Ozcelik <ozceliklara@gmail.com>
2023-07-12 12:00:44 -0400

    Added functionality to dispute facts

--- src/App.js
+++ src/App.js
@@ -229,6 +229,8 @@ function FactList({ facts, setFacts }) {
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -262,6 +264,7 @@ function Fact({ fact, setFacts }) {
     <li className="fact-box">
       <div className="fact">
         <p>
+          {isDisputed ? <span className="disputed">[🚫 DISPUTED] </span> : null}
           {fact.text}
           <a
             className="source"
